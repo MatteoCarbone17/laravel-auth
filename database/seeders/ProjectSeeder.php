3e214939d1650c90b1800c28d3e6776fc8a9725b
MatteoCarbone17 <matteocarbone17@live.com>
2023-02-20 17:40:13 +0100

    add faker

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\project as ModelsProject;
+use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use SebastianBergmann\CodeCoverage\Report\Xml\Project;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +16,17 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 25; $i++) { 
+            $newProject = new ModelsProject();
+            $newProject->title = $faker->unique()->realTextBetween(5,10);
+            $newProject->author =$faker->realTextBetween(3,10);
+            $newProject->slug = Str::slug($newProject->title);
+            $newProject->content =$faker->realTextBetween(500,900);
+            $newProject->project_date_start =$faker->DateTimeThisYear();
+            $newProject->project_date_end =$faker->DateTimeThisYear();
+            $newProject->save();
+        }
     }
 }
