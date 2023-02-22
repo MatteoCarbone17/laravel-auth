547724d10b9667b52892eadccc17128fc22f7709
MatteoCarbone17 <matteocarbone17@live.com>
2023-02-22 16:27:55 +0100

    add layout

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -3,46 +3,55 @@
 @section('content')
 <div class="container">
   <div class="row">
-    {{-- @dump( $previousProject) --}}
     <div class="col">
-        <div class="card p-5">
-            <div class="card-header p-3">
-              {{ $project->author }}
+      <div class="card p-5">
+        <div class="card-header d-flex  bg-success text-light justify-content-between p-3">
+          <p>
+              Author :  {{ $project->author }}
+          </p>
+          <span>
+              <i class="fa-solid fa-envelopes-bulk"></i>
+          </span>
+          <p>
+             User:  {{ Auth::user()['name'] }}
+          </p>
+      </div>
+        <div class="card-body text-center">
+          <h5 class="card-title">{{ $project->title }}</h5>
+          <p class="card-text p-3">{{ $project->content }}</p>
+          <div class="card-footer p-3">
+                <span class="d-block">Data inizio : {{ $project->project_date_start }} </span>
+                @if (isset($project->project_date_end))
+                    <p> Data fine: {{ $project->project_date_end }}</p>
+                @else
+                    <p> Project working in progress </p>
+                @endif
+                <span> Slug: {{ $project->slug }} </span>
+          </div>
+          <div class="row mt-5">
+            <div class="col-4">
+              @if (isset($previousProject->id))
+                  <a class="btn btn-outline-primary mt-3"
+                      href="{{ route('admin.projects.show', $previousProject->id) }}">Previous Page</a>
+              @else
+                  <a class="btn btn-outline-primary disabled mt-3" href="">End Previous Page</a>
+              @endif
             </div>
-            <div class="card-body text-center">
-              <h5 class="card-title">{{ $project->title }}</h5>
-              <p class="card-text">{{ $project->content }}</p>
-              <div class="card-footer p-3">
-               <span class="d-block">Data inizio : {{ $project->project_date_start }} </span>
-               @if(isset($project->project_date_end) )
-               <p> Data fine:  {{  $project->project_date_end}}</p>
-               @else
-                 <p> Project working in progress </p>  
-               @endif
-               <span> Slug:  {{ $project->slug }} </span>
-              </div>
-              <div class="row mt-5">
-                <div class="col-4">
-                  @if (isset($previousProject->id))
-                  <a class="btn btn-outline-primary mt-3"  href="{{ route('admin.projects.show' , $previousProject->id) }}">Previous Page</a>
-                  @else
-                  <a class="btn btn-outline-primary disabled mt-3"  href="">End Previous Page</a>
-                  @endif
-                </div>
-                <div class="col-4">
-                  <a class="btn btn-success mt-3"  href="{{ route('admin.projects.index' , $project->id) }}">Return to Project Page</a>
-                </div>
-                <div class="col-4">
-                  @if (isset($nextProject->id))
-                  <a class="btn btn-outline-primary mt-3"  href="{{ route('admin.projects.show' , $nextProject->id) }}">Next Page</a>
-                  @else
-                  <a class="btn btn-outline-primary disabled mt-3"  href="">End Next Page</a>
-                  @endif
-                </div>
-                {{-- {{ $projects->links()}} --}}
-              </div>
+            <div class="col-4">
+              <a class="btn btn-success mt-3"
+                  href="{{ route('admin.projects.index', $project->id) }}">Return to Project Page</a>
+            </div>
+             <div class="col-4">
+              @if (isset($nextProject->id))
+                  <a class="btn btn-outline-primary mt-3"
+                      href="{{ route('admin.projects.show', $nextProject->id) }}">Next Page</a>
+              @else
+                  <a class="btn btn-outline-primary disabled mt-3" href="">End Next Page</a>
+              @endif
             </div>
           </div>
+        </div>
+      </div>
     </div>
   </div>
 </div>
--- resources/views/guests/postProjects/index.blade.php
+++ resources/views/guests/postProjects/index.blade.php
@@ -5,17 +5,28 @@
     @foreach ($projects as $project)
     <div class="row p-3">
         <div class="col">
-            <div class="card p-1">
-                <div class="card-header p-3">
+            <div class="card p-1 border-5 ">
+                <div class="card-header d-flex  bg-success text-light justify-content-between p-3">
                     <p>
                         Author :  {{ $project->author }}
+                    </p>
+                    <span>
+                        <i class="fa-solid fa-envelopes-bulk"></i>
+                    </span>
+                    <p>
+                       User:  {{ Auth::user()['name'] }}
                     </p>
                 </div>
                 <div class="card-body text-center">
                     <h5 class="card-title">{{ $project->title }}</h5>
-                    <p class="card-text">{{ $project->content }}</p>
-                    <div class="card-footer p-3">
-                      <span class="d-block">Data inizio : {{ $project->project_date_start }} </span> <span>Data fine:  {{  $project->project_date_end}}</span>
+                    <p class="card-text font-medium p-3 ">{{ $project->content }}</p>
+                    <div class="card-footer p-2">
+                        <span class="d-block">Data inizio : {{ $project->project_date_start }} </span>
+                        @if (isset($project->project_date_end))
+                            <p> Data fine: {{ $project->project_date_end }}</p>
+                        @else
+                            <p> Project working in progress </p>
+                        @endif
                     </div>
                 </div>
             </div>
